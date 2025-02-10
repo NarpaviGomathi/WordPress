affb5f58bb9ad93f30ac55903d1b50566a4a985d
NarpaviGomathi <117557881+NarpaviGomathi@users.noreply.github.com>
2025-02-10 10:03:45 +0530

    Update functions.php

--- wp-content/themes/my-custom-theme/functions.php
+++ wp-content/themes/my-custom-theme/functions.php
@@ -1,16 +1,17 @@
 <?php
-// Enqueue theme styles and scripts
-function my_custom_theme_assets() {
-    wp_enqueue_style( 'my-custom-style', get_stylesheet_uri() );
+// Theme setup
+function mycustomtheme_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'mycustomtheme' ),
+    ) );
 }
-add_action( 'wp_enqueue_scripts', 'my_custom_theme_assets' );
+add_action( 'after_setup_theme', 'mycustomtheme_setup' );
 
-// Register menu locations
-function my_custom_theme_menus() {
-    register_nav_menus(
-        array(
-            'main_menu' => __( 'Main Menu' ),
-        )
-    );
+// Enqueue styles and scripts
+function mycustomtheme_scripts() {
+    wp_enqueue_style( 'mycustomtheme-style', get_stylesheet_uri() );
 }
-add_action( 'after_setup_theme', 'my_custom_theme_menus' );
+add_action( 'wp_enqueue_scripts', 'mycustomtheme_scripts' );
+?>
