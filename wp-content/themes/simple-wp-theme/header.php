9166233080528b800e636d2cc18199116f5a87c3
NarpaviGomathi <117557881+NarpaviGomathi@users.noreply.github.com>
2025-04-08 16:28:36 +0530

    Update header.php

--- wp-content/themes/simple-wp-theme/header.php
+++ wp-content/themes/simple-wp-theme/header.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(''); ?></title>
+    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<header>
-    <nav>
-        <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
-    </nav>
-</header>
+    <header>
+        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+        <p><?php bloginfo('description'); ?></p>
+    </header>
