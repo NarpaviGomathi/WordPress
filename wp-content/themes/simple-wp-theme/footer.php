3f8be39d01646009d2c3c4cee3d334133a953244
NarpaviGomathi <117557881+NarpaviGomathi@users.noreply.github.com>
2025-04-08 16:28:58 +0530

    Update footer.php

--- wp-content/themes/simple-wp-theme/footer.php
+++ wp-content/themes/simple-wp-theme/footer.php
@@ -1,6 +1,7 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Simple WP Theme</p>
-</footer>
-<?php wp_footer(); ?>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> - Sample Theme</p>
+    </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
+
